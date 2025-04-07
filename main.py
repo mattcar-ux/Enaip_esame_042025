19a83cd6e17faa83f27108a869bcd17f55483ffb
jthoma92 <jthoma92@gmail.com>
2025-04-07 17:39:13 +0200

    Add files via upload

---
+++ main.py
@@ -0,0 +1,10 @@
+"""Nel file main.py del progetto:
+Bisogna scrivere:
+1. funzione che carica il file test.json dalla cartella dati per :
+    contare quanti dizionari hanno la proprietà: attivo == true
+2. Creare Oggetti del tipo Prodotti per ogni Prodotto nel test.json
+3. Creare un oggetto del tipo Ordine che contiene ogni Prodotto del test.json con attivo == true
+4.Caricare un file testo dal link, salvandolo nella cartella dati:
+https://www.gutenberg.org/files/1342/1342-0.txt
+
+"""
